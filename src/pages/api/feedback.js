a3639253733f18066323c6578853edc809948f70
qasimzaib <zaib_qasim@hotmail.com>
2023-02-03 00:13:22 +0500

    Basic form submission

---
+++ src/pages/api/feedback.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+	if (req.method === "POST") {
+		const { email, text } = req.body;
+		const newFeedback = {
+			id: new Date().toISOString(),
+			email: email,
+			text: text,
+		};
+
+		const filePath = path.join(process.cwd(), "data", "feedback.json");
+		const fileData = fs.readFileSync(filePath);
+		const data = JSON.parse(fileData);
+		data.push(newFeedback);
+		fs.writeFileSync(filePath, JSON.stringify(data));
+
+		res.status(201).json({ success: true, feedback: newFeedback });
+	} else {
+		res.status(200).json({ success: true });
+	}
+}
